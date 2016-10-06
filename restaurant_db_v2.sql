a651885c18b3db41d610767064284643068a7e48
Dominic Zenon <dominiczenon@gmail.com>
2016-10-06 19:29:03 -0400

    added

--- restaurant_db_v2.sql
+++ restaurant_db_v2.sql
@@ -17,10 +17,7 @@ restaurant.id
 where
   restaurant.name = 'Natalie Thai';
 
-
-
-
-
+--another way of doing same problem
 
 select
     *
@@ -30,18 +27,6 @@ from
 where
     review.restaurant_id =
     restaurant.id and restaurant.name = 'Natalie Thai';
-
-
-select
-    *
-from
-    review
-inner join restaurant
-    on review.restaurant_id = restaurant.id
-where
-    restaurant.name = 'Natalie Thai';
-
-;
 
 
 --get the average stars by restaurant. (restaurant name, average star rating)
@@ -80,3 +65,64 @@ from
     restaurant, review, reviewer
 where
     review.restaurant_id = restaurant.id and review.reviewer_id = reviewer.id;
+
+    --get the average stars by reviewer (reviewer name, average star rating)
+
+    select
+    round(avg(review.star), 2),
+    reviewer.name
+    from
+    review
+    inner join reviewer
+    on review.reviewer_id =
+    reviewer.id group by
+    reviewer.name;
+
+    --get the lowest star rating for each reviewer (reviewer name, lowest star rating)
+
+    select
+  min(review.stars),
+  reviewer.name
+  from
+  review
+  inner join reviewer
+  on review.reviewer_id = reviewer.id group by
+  reviewer.name;
+
+  --get the number of restaurants in each category (category name, restaurant count)
+
+  select
+  count(*),
+  food_category
+  from
+  restaurant group by
+  restaurant.food_category;
+
+
+  --get number of 5 star reviews by restaurant (restaurant name, 5-star count)
+
+  select
+  count(review.stars = 5),
+  restaurant.name
+  from
+  review
+  inner join restaurant
+  on review.restaurant_id =
+  restaurant.id for group by
+  restaurant.name;
+  --count doesnt work right
+
+
+
+
+ --average star rating for a food category (category name, average star rating)
+
+ select
+ round(avg(review.stars), 2),
+ restaurant.food_category
+ from
+ review
+ inner join restaurant
+ on review.restaurant_id =
+ restaurant.id group by
+ restaurant.food_category;
